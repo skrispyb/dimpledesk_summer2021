0d8a0d6bcc55f3ea20783154a8eb04aead674b56
skrispy <skrispyb@gmail.com>
2021-05-28 12:52:06 -0700

    added logo and changed name

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,14 @@
 import React from "react";
 
-import logo from "../assets/logo.svg";
+import logo from "../assets/dimpledesk.svg";
 
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+    <h1 className="mb-4">Welcome to DimpleDesk</h1>
 
     <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
+      DimpleDesk is a school project by team Blue7, using <a href="https://reactjs.org">React.js</a>
     </p>
   </div>
 );
